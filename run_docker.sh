1f4a5eaa06dbca5d381228ab3638326ea25bfe95
Glory <olusolagloryolamide@gmail.com>
2025-11-12 20:09:36 +0200

    Add run_docker.sh script for running Docker container

---
+++ run_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+xhost +local:root > /dev/null
+
+docker run -it --rm \
+  --gpus all \
+  --net=host \
+  -v /tmp/argus_socket:/tmp/argus_socket \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v "$HOME/.Xauthority:/root/.Xauthority:ro" \
+  -e DISPLAY="$DISPLAY" \
+  -e PLATFORM_OVERRIDE=jetson \
+  -e GST_DEBUG=3 \
+  -v "$(pwd)":/app \
+  barcode-detector
